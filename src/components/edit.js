bfb04e4ddd512ae3b3ac7bbcb5e1d1f40f732233
LuqmanFarooq <luqifarooq@gmail.com>
2019-11-07 14:28:59 +0000

    first commit

---
+++ src/components/edit.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Edit extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Hello from Edit</h1>
+            </div>
+        )
+    }
+}
+
+export default Edit;
